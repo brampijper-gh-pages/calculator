171cbbbded42c23461fbb8f4bb5694e84c874eb9
brampijper <brampijper@gmail.com>
2019-06-30 11:25:09 +0200

    Created basic functions for the calculator

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-console.log(' Hi from the console ')
+
+function operate(operator, num1, num2) {
+
+}
+
+function add(num1, num2) {
+    return num1 + num2; 
+}
+
+function substract(num1, num2) {
+    return num1 - num2; 
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide (num1, num2) {
+    return num1 * num2;
+}
+
+
+// switch(calculator) {
+//     case add: 
+//         //
+//         break;
+//     case substract:
+//         //
+//         break;
+//     case multiply:
+//         //
+//         break;
+//     case divide:
+//         //
+//         break;
+//     default:
+//         return;
+// }
